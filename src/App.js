03481d1e18282e1da2f7bec7699c11f0de811359
mohammedpythons <mohammedshokai99@gmail.com>
2022-01-28 22:11:12 -0500

    setting up the routess

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/profile" element={<Signin />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYfKeLd8f4buZuwvk82_B6Wew3N6RjRPw",
+  authDomain: "house-marketplace-a4348.firebaseapp.com",
+  projectId: "house-marketplace-a4348",
+  storageBucket: "house-marketplace-a4348.appspot.com",
+  messagingSenderId: "192649447572",
+  appId: "1:192649447572:web:17ad675363376ddf528c81",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Initialize firestore
+
+export const db = getFirestore();
