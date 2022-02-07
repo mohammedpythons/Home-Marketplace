3cd00994e59f4d70c28c17826c7be51775ff167a
mohammedpythons <mohammedshokai99@gmail.com>
2022-02-07 11:37:05 -0500

    add category page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Navbar from "./components/Navbar";
+import Category from "./pages/Category";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./components/routing/PrivateRoute";
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return <div className='explore'>
+    <header>
+      <p className='pageHeader'>Explore</p>
+    </header>
+
+    <main>
+      {/* sliders */}
+      <p className="exploreCategoryHeading">Categories</p>
+      <div className="exploreCategories">
+        <Link to='/category/sell'>
+          <img src={rentCategoryImage} alt="rentimage" className='exploreCategoryImg' />
+        <p className="exploreCategoryName">Places for Rent</p>
+        </Link>
+        <Link to='/category/sell'>
+          <img src={sellCategoryImage} alt="sellimage" className='exploreCategoryImg' />
+          <p className="exploreCategoryName">Places for Sell</p>
+        </Link>
+      </div>
+    </main>
+  </div>;
 };
 
 export default Explore;
