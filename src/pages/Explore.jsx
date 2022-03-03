92faf64a735c1a15b418e983379361cf09084716
mohammedpythons <mohammedshokai99@gmail.com>
2022-03-03 14:40:01 -0500

    add slide to explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+import Slider from '../components/Slider';
 
 const Explore = () => {
   return <div className='explore'>
@@ -10,7 +11,7 @@ const Explore = () => {
     </header>
 
     <main>
-      {/* sliders */}
+      <Slider />
       <p className="exploreCategoryHeading">Categories</p>
       <div className="exploreCategories">
         <Link to='/category/rent'>
